3b615e0ae12f82ecf8f3f218f7db72fdedbe4119
Will W <will.winston@gmail.com>
2022-08-15 15:23:00 -0400

    Completed all of the core functionality, saving descriptions when clicking on the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,93 @@
+
 var today = moment();
 var dayWeek = today.format("dddd")
 $("#currentDay").text(dayWeek + ", " + today.format("MMM Do"));
+var container = document.getElementById("container"); 
 
-let container = document.getElementById("container"); 
+// console.log(localStorage.getItem('simpleCalendar'));
+
+//initialize local storage if it doesn't exist create it, else import object from local storage
+var obj = {};
+if (localStorage.getItem('simpleCalendar') === null){
+    obj = {
+        9: "",
+        10: "",
+        11: "",
+        12: "",
+        13: "",
+        14: "",
+        15: "",
+        16: "",
+        17: ""
+    };
+}
+else {
+    obj = JSON.parse(window.localStorage.getItem('simpleCalendar'));
+
+}
+
 
 //set header of table
-var time = ""
+var time = "";
+var colorScheme = "";
 let table = `
 <table class="table timebox">
   <thead></thead>
-  <tbody>
+  <tbody id=tableBody>
   `;
+  
   //create//append rows
-  //<path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"></path>
-  //viewBox="0 0 16 16"
-  //width="16" height="16" 
   for(i = 9; i < 18; i++){
+    var rowEl = $('tbody');
+    var cellData = "";
+     
+    //Using 24 hour time to measure if something is in the past or future
+    if (i < moment().format('H')) {
+        colorScheme = "past";
+    }
+    else if (i > moment().format('H')){
+        colorScheme = "future";
+    }
+    else {
+        colorScheme = "present";
+    }
+
+    //set the time to 12 hour
     if (i < 13) {
         time = i + " AM";
     }
     else {
         time = (i - 12) + " PM";
     } 
+
+    //Check if anything is in local storage for each row    
+    if (obj[i]){
+        cellData = obj[i];
+    }
+
+    //create rows in table
     table = table +
     `<tr class="row">
       <td class="col-1 hour">` + time + `</td>
-      <td class="col-7" contenteditable="true"></td>
-      <td class="col-1 lastCell"><button type="button" class="saveBtn btn-block"><i class="fas fa-lock"></i></button></td>
+      <td class="col-8 textField ` + colorScheme + `"><textarea  id="field` + i + `">` + cellData + `</textarea></td>
+      <td class="col-1 lastCell"><button type="button" class="saveBtn btn-block" id="lock-btn` + i + `"><i class="fas fa-lock"></i></button></td>
     </tr>`;
+
 }
 
-// let grid = `<div class="timebox">`;
-// for(i = 9; i < 18; i++){
-//     grid = grid + 
-//     `<div class="row">
-//         <div class="col-1">1 of 3</div>
-//         <div class="col-5">2 of 3</div>
-//         <div class="col-1"><button type="button" class="saveBtn"></button>
-//         </div>
-//     </div>`
-// }
-//close off table
 table = table + `</tbody></table>`;
 container.innerHTML = table;
-// grid = grid + `</div>`;
-// container.innerHTML = grid;
+
+
+var eachButton = "";
+for (let i=9;i < 18;i++) {
+
+    // console.log(lockButton);
+    $('#lock-btn'+i).on('click', function () {
+        var field = $('#field'+i);
+        obj[i] = field[0].innerHTML;
+        localStorage.setItem("simpleCalendar", JSON.stringify(obj))
+        
+    });
+}
+
